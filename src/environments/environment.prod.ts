635b3c847b80c4c8357ed98067ecac1f3b051315
Samir Méndez <samimmendez@hotmail.es>
2021-08-29 11:38:24 -0400

    The project structure was builded and the tools and media files necessary was added successfully.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDvZGqh56iJ-yu6dfn9GJ7BOgaTpj-02jg',
+    authDomain: 'buiness-clients.firebaseapp.com',
+    databaseURL: 'https://buiness-clients-default-rtdb.firebaseio.com',
+    projectId: 'buiness-clients',
+    storageBucket: 'buiness-clients.appspot.com',
+    messagingSenderId: '487199550104',
+    appId: '1:487199550104:web:006b90d0e3749c0aa9feed',
+    measurementId: 'G-K1N1ZZNFQT'
+  }
 };
